e498ccce31cd3f61cf4fbde1759ebd975417efaf
Sigma88 <sigmamodexpansions@outlook.com>
2017-09-24 22:30:12 +0200

    test successful (#37)
    * small improvements and contractWeight
    
    * not required anymore
    
    * contractWeight
    
    * fix logspam and contracts
    
    * not needed
    
    * avoid infinites
    
    * recompile
    
    * added version.cs
    
    * recompile
    
    * changelog and version
    
    * remove contracts crap from the plugin
    
    * now it's not my problem anymore
    
    * recompile
    
    * new version log and recompile
    
    * test
    
    * some more fixes
    
    * compile

--- [Source]/SigmaBinary/PeriodFixer.cs
+++ [Source]/SigmaBinary/PeriodFixer.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 namespace SigmaBinaryPlugin
 {
-    public class PeriodFixer : MonoBehaviour
+    [KSPAddon(KSPAddon.Startup.Instantly, true)]
+    public class PeriodFxixer : MonoBehaviour
     {
-        void Start()
+        public void Start()
         {
-            foreach (CelestialBody cb in FlightGlobals.Bodies)
+            Kopernicus.Events.OnPostFixing.Add(FixPeriods);
+        }
+
+        public void FixPeriods()
+        {
+            var bodies = FlightGlobals.Bodies;
+
+            for (int i = 0; i < bodies?.Count; i++)
             {
+                CelestialBody cb = bodies[i];
+
                 if (SigmaBinary.periodFixerList.ContainsKey(cb.transform.name))
                 {
+                    Debug.Log("PeriodFixer", "Fixing orbital period of body " + cb + ", old orbital period = " + cb.orbit.period);
+
+                    cb.orbit.orbitalSpeed *= cb.orbit.period;
                     cb.orbit.period = SigmaBinary.periodFixerList[cb.transform.name];
                     cb.orbit.meanMotion = 2 * Math.PI / cb.orbit.period;
                     cb.orbit.ObTAtEpoch = cb.orbit.meanAnomalyAtEpoch / 2 / Math.PI * cb.orbit.period;
+                    cb.orbit.orbitalSpeed /= cb.orbit.period;
+                    Debug.Log("PeriodFixer", "Fixed orbital period of body " + cb + ", new orbital period = " + cb.orbit.period);
+
                     if (cb.tidallyLocked)
+                    {
                         cb.rotationPeriod = cb.orbit.period;
-
-
-                    SigmaBinary.periodFixerList.Remove(cb.transform.name);
+                        Debug.Log("PeriodFixer", "Fixed rotation period of tidallyLocked body " + cb + ", new rotation period = " + cb.rotationPeriod);
+                    }
                 }
             }
         }
